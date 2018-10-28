c8511be783486e931f37a7dffe20d0c0881a6d7e
Alex <alexjakuba@seznam.cz>
2018-10-28 12:11:52 +0100

    another commit

--- Alex_blog_script.js
+++ Alex_blog_script.js
@@ -1,1 +1,8 @@
-f
+
+$('button').on('click', (event)  => {
+
+$('.posts').append(`<h1>${$('input').val()}</h1> <p>${$('.first_textarea').val()}<//p>`);
+})
+$('ul').on('click',(event) => {
+    $(event.target).css('text-decoration', 'line-through')
+})
